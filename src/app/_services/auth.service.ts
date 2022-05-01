ef7ed7e2d2c95a926fc42c3cc387df90abf940a9
Frostbite22 <landoulsiferes@outlook.com>
2022-05-01 12:36:05 +0100

    login jwt

---
+++ src/app/_services/auth.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const AUTH_API = 'http://localhost:8090/auth/' ;
+const httpOptions = { headers : new HttpHeaders({'Content-Type': 'application/json'})
+}; 
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http : HttpClient) { }
+  
+  login (login : string , password: string): Observable<any>
+  {
+    return this.http.post(AUTH_API+'signin',{
+      login,password
+    },httpOptions)
+  }
+
+  register (login : string, email: string, password : string) : Observable<any>
+  {
+    return this.http.post(AUTH_API+'signup',{
+      login,email,password
+    },httpOptions)
+  }
+}
